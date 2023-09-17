946b67a5d5cbeda51d665169760a720335cf1451
Donaël Walter <donael.walter@hotmail.com>
2023-09-17 15:00:48 +0200

    Improve about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,43 @@ import { defineConfig } from 'astro/config';
 // Add React support
 import react from '@astrojs/react';
 
+//
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const pathSrc = path.resolve(__dirname, "./src").replace(/\\/g, "/");
+
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
+    vite:{
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import "${pathSrc}/assets/styles/root.scss";`
+                }
+            }
+        }
+    },
+    plugins: [
+        // Import your global SCSS file here
+        {
+          name: 'import-global-scss',
+          config() {
+            return {
+              resolve: {
+                alias: {
+                  // Use an alias to import your global SCSS file
+                  'global-scss': path.resolve(__dirname, 'src/assets/styles/root.scss').replace(/\\/g, "/"),
+                },
+              },
+            };
+          },
+        },
+      ],
 });
