6dec0acd965fb7d990dd00b55837d933b3a8e0ae
Donaël WALTER <donael.walter@hotmail.com>
2024-03-19 13:44:51 +0100

    Big Update Portfolio

--- src/components/molecules/ProjectList/index.tsx
+++ src/components/molecules/ProjectList/index.tsx
@@ -1,13 +1,8 @@
 import Style from './style.module.scss';
 import ProjectItem from '../ProjectItem';
-const ProjectList = (props: {projects: any[]}) => {
-    const {projects} = props;
+const ProjectList = (props: {projects: any[] }) => {
+    const {projects = []} = props;
     return (<ul className={Style.root}>
-        {
-            !projects && [...new Array(200)].map((el, index)=> {
-                return <li><ProjectItem index={index}/></li>
-            })
-        }
                 {
             projects && projects.map((el, index)=> {
                 return <li><ProjectItem index={index} title={el.name + ' - ' + el.category} picture={el.cover} link={'/projects/'+ el.category + '/' + el.seoSlug.current}/></li>
