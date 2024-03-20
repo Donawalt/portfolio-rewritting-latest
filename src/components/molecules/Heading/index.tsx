de78ef31b0ceda7a71af8f5470bf63debdf11f3f
Donaël WALTER <donael.walter@hotmail.com>
2024-03-20 01:51:14 +0100

    Change stuffs

--- src/components/molecules/Heading/index.tsx
+++ src/components/molecules/Heading/index.tsx
@@ -6,7 +6,7 @@ import eventBus from '@/assets/scripts/utils/eventBus';
 const Heading = (props: { top?, children: any, subtitle? }) => {
     const { top, children, subtitle } = props;
     useEffect(()=>{
-        gsap.fromTo(`.${Style.root} [data-astro-source-file], [data-hero-project-image], .do_landing_content, .do_project_content, .do_project_hero`, {opacity:0, translateY: '16px', delay: '400ms'}, {duration: 0.6, opacity:1, stagger: 0.1, translateY: 0, ease: "power3.out", onComplete: ()=>{
+        gsap.fromTo(`.${Style.root} [data-astro-source-file], .${Style.root} astro-slot> *,  [data-hero-project-image], .do_landing_content, .do_project_content, .do_project_hero`, {opacity:0, translateY: '16px', delay: '400ms'}, {duration: 0.6, opacity:1, stagger: 0.1, translateY: 0, ease: "power3.out", onComplete: ()=>{
             eventBus.dispatch('scroll');
         }});
     }, [])
