1317176f1809e4954bdfca9416f2382e74cfbf88
Donaël WALTER <donael.walter@hotmail.com>
2024-03-29 22:01:32 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,45 +6,42 @@ import react from '@astrojs/react';
 //
 import path from "path";
 import { fileURLToPath } from 'url';
-
+import sitemap from "@astrojs/sitemap";
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
-
 const pathSrc = path.resolve(__dirname, "./src").replace(/\\/g, "/");
 
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
-    vite:{
-      resolve: {
-        alias: {
-          "@/": `${path.resolve(__dirname, "src")}/`,
-        },
-      },
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `@import "${pathSrc}/assets/styles/root.scss";`
-                }
-            }
-        }
+  integrations: [react(), sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`
+      }
     },
-    plugins: [
-        // Import your global SCSS file here
-        {
-          name: 'import-global-scss',
-          config() {
-            return {
-              resolve: {
-                alias: {
-                  // Use an alias to import your global SCSS file
-                  'global-scss': path.resolve(__dirname, 'src/assets/styles/root.scss').replace(/\\/g, "/"),
-                },
-              },
-            };
-          },
-        },
-      ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "${pathSrc}/assets/styles/root.scss";`
+        }
+      }
+    }
+  },
+  plugins: [
+  // Import your global SCSS file here
+  {
+    name: 'import-global-scss',
+    config() {
+      return {
+        resolve: {
+          alias: {
+            // Use an alias to import your global SCSS file
+            'global-scss': path.resolve(__dirname, 'src/assets/styles/root.scss').replace(/\\/g, "/")
+          }
+        }
+      };
+    }
+  }]
 });
